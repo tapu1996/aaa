26610355ccab8876adfaecd5aa1af64642f31f90
tapu1996 <tapotoshghosh@gmail.com>
2018-04-25 01:10:22 +0600

    commmmm

---
+++ src/abc/Servlet.java
@@ -0,0 +1,23 @@
+package abc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Servlet", urlPatterns = "/abc"  )
+
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter p = response.getWriter();
+        p.println("<html><body>ads</body></html>");
+
+    }
+}
